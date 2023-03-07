8e7cd3c9865c64f8ae1cc896ff0f2838fecadbcf
MatveyRomodanovskiy <startpolus@gmail.com>
2023-03-08 00:56:20 +0300

    hw-32

---
+++ src/components/Timer.tsx
@@ -0,0 +1,29 @@
+import { type } from "@testing-library/user-event/dist/type";
+import timeZones from "../time-zones";
+import React, { useEffect } from "react";
+type Props = {
+    cityCountry: string;
+}
+export const Timer:React.FC <Props> = ({cityCountry}) => {
+    const styles:React.CSSProperties = {background: "lightblue",
+    fontSize: "2em"};
+    const [time, setTime] = React.useState(new Date());
+    function tic(){
+       setTime(new Date());
+    }
+    
+        <p>{new Date().toDateString()}</p>
+   
+
+    useEffect(() => {
+        const interval = setInterval(tic, 1000);
+        return () => clearInterval(interval);
+    },[]);
+    const index : number = timeZones.findIndex(e => e.name.includes(cityCountry) || e.countryName.includes(cityCountry));
+    return <div id="localTime">
+        <h2 id="titleTime">Current time in {cityCountry}</h2>
+        <p id="stringTime" style={styles}>{time.toLocaleTimeString(undefined, {timeZone: timeZones[index].name})}</p>
+    </div>
+}
+
+
