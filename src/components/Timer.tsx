08d0e339b33d27da3b9da542d9f4dfb2ba82566f
MatveyRomodanovskiy <startpolus@gmail.com>
2023-03-09 22:09:09 +0300

    isr

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -19,7 +19,8 @@ export const Timer:React.FC <Props> = ({cityCountry}) => {
         const interval = setInterval(tic, 1000);
         return () => clearInterval(interval);
     },[]);
-    const index : number = timeZones.findIndex(e => e.name.includes(cityCountry) || e.countryName.includes(cityCountry));
+    let index : number = timeZones.findIndex(e => e.name.includes(cityCountry) || e.countryName.includes(cityCountry));
+    index = -1 ? 195 : index;
     return <div id="localTime">
         <h2 id="titleTime">Current time in {cityCountry}</h2>
         <p id="stringTime" style={styles}>{time.toLocaleTimeString(undefined, {timeZone: timeZones[index].name})}</p>
