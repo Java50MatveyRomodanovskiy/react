737329a696634cb2ebc85f837314558260d4a7ce
MatveyRomodanovskiy <startpolus@gmail.com>
2023-03-16 23:37:07 +0300

    edited (deleted divs)

--- src/components/CounterMultiply.tsx
+++ src/components/CounterMultiply.tsx
@@ -6,8 +6,7 @@ type Props = {
 export const CounterMultiply: React.FC<Props> = ({factor}) => {
     const userName = useSelector<any, string>(state=>state.login.userName);
     const counter = useSelector<any, number>(state=>state.count.counter);
-    return <div>
-        {(userName == 'admin') && <p>Counter value is {counter}, factor is {factor}, counter * factor = {counter * factor}</p>}
-    </div>
+    return  <>{(userName == 'admin') && <p>Counter value is {counter}, factor is {factor}, counter * factor = {counter * factor}</p>}</>
+    
 
 }
--- src/components/CounterSquare.tsx
+++ src/components/CounterSquare.tsx
@@ -4,8 +4,8 @@ import {useSelector} from 'react-redux';
 export const CounterSquare: React.FC<any> = () => {
     const userName = useSelector<any, string>(state=>state.login.userName);
     const counter = useSelector<any, number>(state=>state.count.counter);
-    return <div>
+    return <>
         {userName &&<p>Counter value is {counter}, counter ** 2 = {counter ** 2}</p>}
-    </div>
+    </>
 
 }
