b62fd0a01eab5bf0e9597c1c5ca168d08c4c72a6
MatveyRomodanovskiy <startpolus@gmail.com>
2023-03-28 18:37:28 +0300

    lifeGame - final

--- src/service/LifeMatrix.ts
+++ src/service/LifeMatrix.ts
@@ -3,7 +3,7 @@ import { getRandomMatrix } from "../util/random";
 export default class LifeMatrix {
     constructor(private _numbers: number[][]) { }
     get numbers() {
-        if (this._numbers.length = 0){
+        if (this._numbers.length == 0){
             this._numbers = getRandomMatrix(this._numbers.length, this._numbers.length, 0, 1);
         }
         return this._numbers;
