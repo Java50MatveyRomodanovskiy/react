103e592a2bcef9901c84054d6f92604a6997e70c
MatveyRomodanovskiy <startpolus@gmail.com>
2023-05-01 22:27:48 +0300

    final

--- src/components/pages/ShoppingCart.tsx
+++ src/components/pages/ShoppingCart.tsx
@@ -1,20 +1,27 @@
 import { Avatar, Box, Typography } from "@mui/material"
 import {DataGrid, GridColDef, GridViewColumnIcon} from "@mui/x-data-grid"
-import { useMemo } from "react"
+import { useEffect, useMemo } from "react"
 import { useSelector } from "react-redux"
+import { productsService } from "../../config/products-service-config"
 import { ProductType } from "../../model/ProductType"
 import { ShoppingProductType } from "../../model/ShoppingProductType"
+import { productsActions } from "../../redux/productsSlice"
 export const ShoppingCart: React.FC = () => {
     type ShoppingCartType  = ProductType & {count:
         number, sum:number};
     const shopping = useSelector<any,ShoppingProductType[]>(state => state.shoppingState.shopping);
+
     const products: ProductType[] = useSelector<any, ProductType[]>(state => state.productsState.products);
     function getTableData(): ShoppingCartType[]{
-        return shopping.map((p,index) => getColumn(p, index));
+        return shopping.map((p,index) => getColumn(p, index)).filter(p => p.id != "delete");
     }
     function getColumn(p: ShoppingProductType, i: number): ShoppingCartType {
         const curProd: ProductType | undefined  = products.find(pr => pr.id == p.id);
-        const curItem: ShoppingCartType = {id: i +'', title: curProd!.title, category: curProd!.category, unit: curProd!.category, cost: curProd!.cost, image: curProd!.image, count: p.count, sum: curProd!.cost * p.count}
+        let curItem: ShoppingCartType = {id: "delete", title: 'curProd!.title', category: "curProd!.category", unit: "curProd!.category", cost: 0, image: "curProd!.image", count: 0, sum: 0};
+        if (curProd){
+        curItem = {id: i +'', title: curProd!.title, category: curProd!.category, unit: curProd!.category, cost: curProd!.cost, image: curProd!.image, count: p.count, sum: curProd!.cost * p.count}
+        } 
+    
         return curItem;
     }
     const tableData = useMemo(() => getTableData(), [products, shopping]);
@@ -37,4 +44,8 @@ export const ShoppingCart: React.FC = () => {
     </Box>
 }
 
+
+function dispatch(arg0: any) {
+    throw new Error("Function not implemented.")
+}
 
