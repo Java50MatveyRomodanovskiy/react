e7c037688c825ad77c6b66298dd1dd25c33701c3
MatveyRomodanovskiy <startpolus@gmail.com>
2023-05-08 10:53:30 +0300

    final

--- src/components/forms/ProductForm.tsx
+++ src/components/forms/ProductForm.tsx
@@ -1,4 +1,4 @@
-import { Alert, Avatar, Box, Button, CardMedia, Grid, InputLabel, MenuItem, NativeSelect, Select, Snackbar, TextField } from "@mui/material";
+import { Alert,  Box, Button, CardMedia, Grid, MenuItem, Select, Snackbar, TextField } from "@mui/material";
 import React, { useState, useRef } from "react";
 import { useSelector } from "react-redux";
 import { CategoryType } from "../../model/CategoryType";
@@ -7,7 +7,7 @@ import productsParameters from "../../config/products-parameters-config.json"
 type Props = {
     submitFn: (product: ProductType) => string
 }
-const initialProduct: ProductType = { category: '', image: '', cost: 0, title: ' ', unit: '' };
+const initialProduct: ProductType = { category: '', image: '', cost: 0, title: '', unit: '' };
 export const ProductForm: React.FC<Props> = ({ submitFn }) => {
     const [open, setOpen] = useState<boolean>(false);
     const alertMessage = useRef<string>('');
@@ -18,18 +18,33 @@ export const ProductForm: React.FC<Props> = ({ submitFn }) => {
         product.title = product.title.trim()
         alertMessage.current = submitFn(product);
         if (!alertMessage.current) {
-            document.querySelector('form')!.reset(); 
-        }else{
+            setProduct({ category: '', image: '', cost: 0, title: '', unit: '' });
+        } else {
             setOpen(true);
         }
     }
+    function handleReset(event: any){
+        setProduct({ category: '', image: '', cost: 0, title: '', unit: '' });
+    }
     function imageHandler(event: any) {
         const urlImage = event.target.value;
-        setProduct({ ...product, image: urlImage });
+        // try{
+        //     const isExist = await fetch(urlImage);
+        //     if (isExist.ok){
+                setProduct({ ...product, image: urlImage });
+        //     } else {
+        //         product.image = '';
+        //         setProduct({ ...product, image: '' });
+        //         throw new Error (isExist.statusText);
+        //     }
+        // }catch (e) {
+        //     alert (e);
+        // }
+       
     }
     function titleHandler(event: any) {
         const titleNew = event.target.value;
-        setProduct({ ...product, title: titleNew});
+        setProduct({ ...product, title: titleNew });
     }
     function categoryHandler(event: any) {
         const categoryNew = event.target.value;
@@ -44,7 +59,7 @@ export const ProductForm: React.FC<Props> = ({ submitFn }) => {
         setProduct({ ...product, cost: costNew });
     }
     return <Box>
-        <form onSubmit={onSubmitFn}>
+        <form id="addProduct" onSubmit={onSubmitFn}>
             <Grid container spacing={4} justifyContent={'center'}>
                 <Grid item xs={8} md={7}>
                     <TextField label='URL image' required fullWidth value={product.image}
@@ -59,16 +74,29 @@ export const ProductForm: React.FC<Props> = ({ submitFn }) => {
                         onChange={titleHandler} />
                 </Grid>
                 <Grid item xs={4}>
-                    <Select label="Category"
-                        required fullWidth value={product.category}
+                    <Select labelId="Category"
+                        required
+                        fullWidth
+                        displayEmpty
+                        value={product.category}
                         onChange={categoryHandler}>
+                        <MenuItem value="">
+                            Category
+                        </MenuItem>
                         {categories.map(cat => <MenuItem value={cat.name}>{cat.name}</MenuItem>)}
                     </Select>
+
                 </Grid>
                 <Grid item xs={2}>
                     <Select label="Unit"
-                        required fullWidth value={product.unit}
+                        required
+                        fullWidth
+                        displayEmpty
+                        value={product.unit}
                         onChange={unitHandler}>
+                        <MenuItem value="">
+                            Unit
+                        </MenuItem>
                         {productsParameters.units.map(un => <MenuItem value={un}>{un}</MenuItem>)}
                     </Select>
                 </Grid>
@@ -81,18 +109,18 @@ export const ProductForm: React.FC<Props> = ({ submitFn }) => {
                         }} />
                 </Grid>
                 <Grid>
-                <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
-                    <Alert severity="error" sx={{ width: '30vw', fontSize: '1.5em' }}>
-                        {alertMessage.current}
-                    </Alert>
-                </Snackbar>
+                    <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
+                        <Alert severity="error" sx={{ width: '30vw', fontSize: '1.5em' }}>
+                            {alertMessage.current}
+                        </Alert>
+                    </Snackbar>
                 </Grid>
                 <Grid item container spacing={5} justifyContent={'center'} xs={12}>
                     <Grid item xs={6}>
                         <Button type="submit">Submit</Button>
                     </Grid>
                     <Grid item xs={6}>
-                        <Button type="reset">Reset</Button>
+                        <Button type="reset" onClick={handleReset}>Reset</Button>
                     </Grid>
                 </Grid>
             </Grid>
